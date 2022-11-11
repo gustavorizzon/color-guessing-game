3be46eab7adcc275089b73045302a03129989c2e
Gustavo Pertuzzatti Rizzon <gustavop.rizzon@gmail.com>
2022-11-11 14:12:45 -0300

    feat: color guessing game

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const Result = Object.freeze({
+  Correct: 'Correct',
+  Wrong: 'Wrong'
+})
+
+const getRandomColor = () => {
+  const randomHex = Math.floor(16**6 * Math.random())
+    .toString(16)
+    .padStart(6, '0')
+    .toUpperCase();
+
+  return `#${randomHex}`
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [correctColor, setCorrectColor] = useState(() => getRandomColor())
+  const [guessResult, setGuessResult] = useState('')
+  const [answers, setAnswers] = useState([])
+
+  const verifyAnswer = (guess) => {
+    if (guess === correctColor) {
+      setGuessResult(Result.Correct)
+      setCorrectColor(getRandomColor())
+    } else {
+      setGuessResult(Result.Wrong)
+    }
+  }
+
+  useEffect(() => {
+    setAnswers(
+      [correctColor, getRandomColor(), getRandomColor()]
+      .sort(() => Math.random() - 0.5)
+    )
+  }, [correctColor])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="game">
+      <h1>Color Guessing Game</h1>
+      <div className="color-container" style={{ backgroundColor: correctColor }}/>
+      <div className="answer-buttons">
+        {answers.map(color => (
+          <button key={color} type="button" onClick={() => verifyAnswer(color)}>{color}</button>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {guessResult === Result.Wrong && (
+        <p className="result wrong">Wrong Answer</p>
+      )}
     </div>
   )
 }
